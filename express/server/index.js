881a61c8d80a72f3936e3862dbdcd7535e796a0a
ROG <luan.roger.2003@gmail.com>
2025-06-14 11:14:02 -0300

    feat: create and read messages
    - Configure CORS

--- express/server/index.js
+++ express/server/index.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
 
-app.get('/:id', (req, res) => {
-  res.send('Hello World!')
-})
+const messages = [];
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/mensagens", (req, res) => {
+  if (messages.length === 0) {
+    res.status(404).send("No messages found");
+    return;
+  }
+
+  res.json(messages);
+});
+
+app.post("/mensagens", (req, res) => {
+  if (!req.body) {
+    res.status(400).send("Bad Request: Body is missing");
+    return;
+  }
+
+  const { texto } = req.body;
+  if (!texto) {
+    res.status(400).send("Bad Request: Missing text in body");
+    return;
+  }
+  messages.push({ texto });
+
+  res.json(messages);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
